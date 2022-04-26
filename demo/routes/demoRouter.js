e82fc5c50811d630598cbd58d07a786c2d18e637
lpatto <liam.pattison1999@gmail.com>
2022-04-26 11:23:44 +1000

    Cleaner code

--- demo/routes/demoRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const demoRouter = express.Router()
-
-const demoController = require('../controllers/demoController')
-
-demoRouter.get('/', demoController.getAllDemoData)
-demoRouter.get('/:id', demoController.getDataById)
-module.exports = demoRouter
