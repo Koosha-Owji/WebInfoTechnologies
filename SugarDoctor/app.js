a99a3ce4ac432759ba86ffab8f6664ad2f3d8dc3
lpatto <liam.pattison1999@gmail.com>
2022-04-28 21:33:37 +1000

    Filtered patient data so only the new data is added

--- SugarDoctor/app.js
+++ SugarDoctor/app.js
@@ -33,4 +33,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
