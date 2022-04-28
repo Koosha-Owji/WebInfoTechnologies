876719a20f01631374f533d3938ec930e918592e
Koosha-Owji <koosha.owji@yahoo.com>
2022-04-28 18:05:57 +1000

    Update Glucose.js

--- SugarDoctor/Models/Glucose.js
+++ SugarDoctor/Models/Glucose.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const glucoseSchema = mongoose.Schema({
-  patient_id: {type: String, required: false},
+  patient_id: {type: String, required: true , default: '123'},
   glucoseLevel: { type: Number, required: true },
-  glucoseUpper: { type: Number, required: false },
-  glucoseLower: { type: Number, required: false },
+  glucoseUpper: { type: Number, required: true , default: '150' },
+  glucoseLower: { type: Number, required: true , default: '50'},
   glucoseComment: { type: String, required: false}
 });
 
