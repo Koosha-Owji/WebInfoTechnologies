fd5f7d7a05f867875fa9188344866eb9a4121f29
Justin Bie <bies@student.unimelb.edu.au>
2022-05-06 14:30:05 +1000

    commit

---
+++ public/assets/scripts/openside.js
@@ -0,0 +1,7 @@
+function openSide() {
+    document.getElementById("category").style.width = "250px";
+  }
+  
+function closeSide() {
+    document.getElementById("category").style.width = "0";
+  }
